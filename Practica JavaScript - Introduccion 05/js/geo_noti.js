a35ab1de949f38ab8c6cde309c26b25b8260fea1
Fasutt0 <159182196+Fasutt0@users.noreply.github.com>
2024-11-11 04:03:21 +0000

    Clase 29-10-24

---
+++ Practica JavaScript - Introduccion 05/js/geo_noti.js
@@ -0,0 +1,26 @@
+let info = document.querySelector("#info");
+function obtenerUbicacion(){
+    navigator.geolocation.getCurrentPosition(function(posicion){
+        const lat = posicion.coords.latitude;
+        const lon = posicion.coords.longitude;
+        info.textContent = `Latitud: ${lat} - Longitud: ${lon}`;
+        notificar();
+    },function(error){
+        console.log(error.message);
+    });
+}
+let boton = document.querySelector("#ubicacion");
+boton.addEventListener("click",() => {
+    obtenerUbicacion();
+});
+
+//Notificacion
+const notificar = () => {
+    Notification.requestPermission()
+    .then(permission => {
+        //Si permitio las notificaciones
+        if(permission == 'granted'){
+        new Notification("Coordenadas Obtenidas");
+        }
+    })
+}
